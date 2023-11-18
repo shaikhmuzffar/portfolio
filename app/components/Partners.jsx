34c2beaf3daa5f2ab59cdd06805508d3c8163013
muzzu <muzzueducation@gmail.com>
2023-11-18 13:55:06 +0530

    partners section responsivness done

--- app/components/Partners.jsx
+++ app/components/Partners.jsx
@@ -9,8 +9,8 @@ import gramin from '../assets/gramin.png'
 export default function Partners() {
     return (
         <div className='flex w-10/12 mx-auto flex-col space-y-[30px] items-center justify-center mt-[90px]'>
-            <h1 className='text-[24px] font-[600] text-blackText'>Proud marketing partner with</h1>
-            <div className='flex flex-wrap items-center  justify-center gap-12' >
+            <h1 className='text-lg sm:text-xl font-[600] text-blackText'>Proud marketing partner with</h1>
+            <div className='flex flex-wrap items-center  justify-center gap-6 md:gap-12' >
                 <div className='relative h-8 w-[102px] '>
                     <Image src={spotify} layout='fill' alt='spotify' objectFit='contain' />
                 </div>
