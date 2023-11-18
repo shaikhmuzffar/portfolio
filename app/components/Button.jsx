b9351378bac5d4ae614aad85eda1f3d92289fa58
muzzu <muzzueducation@gmail.com>
2023-11-18 13:59:42 +0530

    button text  issue fixed

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button
  ({children, className}) {
   return (
     <div className=''>
-        <div className={`${className} items-center justify-center rounded-[4px] text-white h-11 w-[150px] text-[12px] bg-myGreen cursor-pointer active:scale-95 transition duration-300 ease-out `}>
+        <div className={`${className} items-center select-none justify-center rounded-[4px] text-white h-11 w-[150px] text-[12px] bg-myGreen cursor-pointer active:scale-95 transition duration-300 ease-out `}>
             {children}
         </div>
     </div>
--- app/components/ButtonSecondary.jsx
+++ app/components/ButtonSecondary.jsx
@@ -3,7 +3,7 @@ import React, { Children } from 'react'
 export default function ButtonSecondary({children,className}) {
   return (
     <div className={`${className}`}>
-        <div className='flex items-center justify-center rounded-[4px] text-[600] border-gray-900 shadow-xl bg-white h-11 w-[150px] text-[12px] text-myGreen hover:bg-gray-50 cursor-pointer active:scale-95 transition duration-300 ease-out '>
+        <div className='flex items-center select-none justify-center rounded-[4px] text-[600] border-gray-900 shadow-xl bg-white h-11 w-[150px] text-[12px] text-myGreen hover:bg-gray-50 cursor-pointer active:scale-95 transition duration-300 ease-out '>
             {children}
         </div>
     </div>
