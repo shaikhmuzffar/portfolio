a6ca76387d89f9ccf14801d0b55628a2b9b4db7a
muzzu <muzzueducation@gmail.com>
2023-11-30 18:04:59 +0530

    some bugs fixed

--- app/components/KeyFeature.jsx
+++ app/components/KeyFeature.jsx
@@ -14,7 +14,7 @@ export default function KeyFeature({ sectionData }) {
                     {/* single keyfeatures  */}
                     {
                         sectionData.map((feature) =>
-                            <div title={feature.title} className='flex space-x-2 items-center w-[350px]  px-3 py-2 '>
+                            <div key={feature.title} className='flex space-x-2 items-center w-[350px]  px-3 py-2 '>
                                 {/* icon */}
                                 <div className='relative h-[60px] opacity-80 w-[60px]'>
                                     <Image className='rounded-xl sm:rounded-none' src={feature.img} alt='screenShot3' layout='fill' objectFit='contain' />
