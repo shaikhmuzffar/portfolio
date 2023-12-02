874d58fa9826e487c70beb061540987762626ef9
muzzu <muzzueducation@gmail.com>
2023-12-02 13:30:07 +0530

    achivements and projectLink section responsiveness done

--- app/components/Achivements.jsx
+++ app/components/Achivements.jsx
@@ -16,15 +16,15 @@ export default function Achivements({sectionData}) {
                 {/* single achivement */}
                 {
                     sectionData && sectionData.map(({title,subTitle}) =>
-                        <div key={title} className='flex space-x-2 pl-3 pb-[30px]'>
-                            <div className='flex  h-[50] w-[50px] justify-center items-center'>
+                        <div key={title} className='flex  space-x-2 pl-3 pb-[30px]'>
+                            <div className='flex  h-[50] w-[50px] pt-3 justify-center items-start lg:items-center'>
                                 <div className='relative h-5 w-5'>
                                     <Image className='rounded-xl sm:rounded-none' src={hashImg} alt='screenShot3' layout='fill' objectFit='contain' />
                                 </div>
                             </div>
-                            <div>
-                                <h1 className='font-semibold' >{title}</h1>
-                                <p className='text-sm opacity-80 ' >{subTitle}</p>
+                            <div >
+                                <h1 className='font-semibold text-base' >{title}</h1>
+                                <p className='text-sm opacity-80' >{subTitle}</p>
                             </div>
                         </div>
                     )
--- app/components/ProjectLinks.jsx
+++ app/components/ProjectLinks.jsx
@@ -14,24 +14,24 @@ export default function ProjectLinks() {
                 <p className='text-sm text-blackText  text-center'>checkout the complete source code and UI/UX design</p>
             </div>
             {/* links */}
-            <div className='grid grid-cols-2 justify-between text-blackText p-2 lg:p-3 xl:p-5 gap-2  xl:gap-5 ' >
+            <div className='grid grid-cols-2 justify-between text-blackText px-10 sm:px-8 md:px-12 lg:p-0  xl:p-5 gap-5 sm:gap-5 xl:gap-4 lg:gap-2 my-8 md:my-10 ' >
                 {/* github */}
-                <div className='col-span-1 active:border-2 border-myGreen active:bg-white  p-2 h-[200px] justify-center  flex flex-col items-center rounded-xl bg-gray-50 cursor-pointer '>
-                    <p className='text-sm font-medium text-center  '>checkout the complete code on</p>
+                <div className='col-span-2  sm:col-span-1 active:border-2  border-myGreen  p-6 lg:p-3 h-[200px] justify-center  flex flex-col items-center rounded-xl bg-gray-50 cursor-pointer select-none active:bg-green-100  '>
+                    <p className='font-medium text-base lg:text-[12px] text-center text-blackText opacity-80 '>checkout the complete source code on</p>
                     {/* Githubicon */}
-                    <div className='relative  mt-5 h-5 w-[90px]  '>
-                        <Image src={githubSmall} layout='fill' alt='githubIcon' objectFit='cover' objectPosition='center' />
+                    <div className='relative  mt-5 h-7 w-[120px] md:h-10 md:w-[140px] lg:w-[90px] lg:h-5'>
+                        <Image src={githubSmall} layout='fill' alt='githubIcon' objectFit='contain' objectPosition='center' />
                     </div>
-                    <p className=' text-sm font-medium mt-2 text-center'>repository</p>
+                    <p className=' font-medium lg:text-[12px] mt-3 text-blackText text-center opacity-80 '>repository</p>
                 </div>
                 {/* dribbble */}
-                <div className='col-span-1 active:border-2 border-myGreen active:bg-white  p-2 sm:p-3 h-[200px] justify-center  flex flex-col items-center rounded-xl bg-gray-50 cursor-pointer '>
-                    <p className='text-sm font-medium text-center '>checkout complete design in my</p>
+                <div className='col-span-2 sm:col-span-1  active:border-2 border-myGreen   p-6 lg:p-3  h-[200px] justify-center  flex flex-col items-center rounded-xl bg-gray-50 cursor-pointer select-none active:bg-green-100 '>
+                    <p className='font-medium text-base lg:text-[12px]  text-center text-blackText opacity-80 '>checkout complete design in my</p>
                     {/* dribbbleicon */}
-                    <div className='relative mt-5 h-5 w-[90px]  '>
-                        <Image src={dribbbleSmall} layout='fill' alt='dribbbleIcon' objectFit='cover' objectPosition='center' />
+                    <div className='relative   mt-5 h-7 w-[120px]  md:h-10 md:w-[140px] lg:w-[90px] lg:h-5 '>
+                        <Image src={dribbbleSmall} layout='fill' alt='dribbbleIcon' objectFit='contain' objectPosition='center' />
                     </div>
-                    <p className='text-sm font-medium mt-2 text-center'>portfolio</p>
+                    <p className='font-medium lg:text-[12px] mt-3 text-blackText text-center opacity-80'>portfolio</p>
                 </div>
 
             </div>
